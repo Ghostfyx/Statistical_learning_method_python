4b5453ec1e326b09ff33061ce9093bb58eae0f31
fanyuexiang <fanyuexiang@jd.com>
2018-12-26 19:43:48 +0800

    KNN模型

--- Chapter Three KNearestNeighbors/KNN.py
+++ Chapter Three KNearestNeighbors/KNN.py
@@ -6,10 +6,13 @@
 # @File    : KNN.py
 # @Software: PyCharm
 import math
+from collections import Counter
+
 import pandas as pd
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 def L(x, y, p=2):
@@ -65,4 +68,47 @@ plt.legend(loc='upper right')
 plt.show()
 data = np.array(df.iloc[:100, [0, 1, -1]])
 X, y = data[:, :-1], data[:, -1]
-print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+class KNN:
+    def __init__(self, X_train, y_train, n_neighbors=3, p=2):
+        """
+        parameter: n_neighbors 临近点个数
+        parameter: p 距离度量
+        """
+        self.n = n_neighbors
+        self.p = p
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X):
+        # 取出n个点
+        knn_list = []
+        for i in range(self.n):
+            dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
+            knn_list.append((dist, self.y_train[i]))
+
+        for i in range(self.n, len(self.X_train)):
+            max_index = knn_list.index(max(knn_list, key=lambda x: x[0]))
+            dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
+            if knn_list[max_index][0] > dist:
+                knn_list[max_index] = (dist, self.y_train[i])
+
+        # 统计
+        knn = [k[-1] for k in knn_list]
+        count_pairs = Counter(knn)
+        max_count = sorted(count_pairs, key=lambda x: x)[-1]
+        return max_count
+
+    def score(self, X_test, y_test):
+        right_count = 0
+        n = 10
+        for X, y in zip(X_test, y_test):
+            label = self.predict(X)
+            if label == y:
+                right_count += 1
+        return right_count / len(X_test)
+
+clf = KNN(X_train, y_train)
+clf.score(X_test, y_test)
